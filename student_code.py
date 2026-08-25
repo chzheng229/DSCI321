9d0266e88b223ec23bb16c4af2ed24ec30c1c837
chzheng229 <chz229@lehigh.edu>
2026-08-25 16:01:18 -0400

    Started working on code
    Defined the function robot_navigation itself, and then began coding the validity/invalidity checks for nodes. 
    i.e: nodes not starting at A, nodes being a list.

---
+++ student_code.py
@@ -0,0 +1,15 @@
+# DSCI321 Coding Assignment 1 - Charlie Zheng
+
+def robot_navigation(nodes):
+    'askdjfhgasldjgkhfj'
+    # Invalidity Checks
+    # Check if nodes is a list
+    if not isinstance(nodes, list):
+        return -1
+
+    # Check if nodes is all strings
+    
+
+    # Check #1, Nodes not starting at A
+    if nodes[0] != 'a':
+        return -1
