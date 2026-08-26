0ba399831a7e5da61109ccdac95b31500f2fb673
chzheng229 <chz229@lehigh.edu>
2026-08-26 13:17:02 -0400

    Nested dict changes + more invalidity checks
    Wrote the weighted direction graph represented by a nested dictionary to create all the paths and weights that exist & added invalidity checks for nodes all being strings and checking if all nodes given in argument list are valid notes.

--- student_code.py
+++ student_code.py
@@ -1,15 +1,34 @@
 # DSCI321 Coding Assignment 1 - Charlie Zheng
 
+GRAPH = {
+    "a": {"b":1, "d":5},
+    "b": {"c":2, "f":5},
+    "c": {"e":1, "h":3},
+    "d": {"f":3},
+    "e": {"d":3, "i":2},
+    "f": {"e":4, "g":3, "i":3},
+    "g": {"h":2, "k":2},
+    "h": {"i":1, "j":2, "z": "4"},
+    "i": {"j":4, "k":2},
+    "j": {"c":1, "k":3, "z":4},
+    "k": {"z":3}
+}
+
 def robot_navigation(nodes):
-    'askdjfhgasldjgkhfj'
-    # Invalidity Checks
-    # Check if nodes is a list
+    """placeholder"""
+    # Invalidity Check
+    #  if nodes is a list
     if not isinstance(nodes, list):
         return -1
 
     # Check if nodes is all strings
-    
+    if not all(isinstance(node, str) for node in nodes):
+        return -1
 
-    # Check #1, Nodes not starting at A
+    # Check if Nodes not starting at A
     if nodes[0] != 'a':
+        return -1
+
+    # Check if all the nodes provided in argument are valid nodes
+    if not all(n in GRAPH for n in nodes):
         return -1
