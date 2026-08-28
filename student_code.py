2f85c5554838f3bdc50fc595a109e1f874a326a0
chzheng229 <chz229@lehigh.edu>
2026-08-28 15:39:26 -0400

    Test code & updates to real code
    Added a test code file to work w test cases. Also addressed a few bugs with original code such as z not being a top level key in dict causing errors, adding empty list invalidity check, and fixing a few pylint warnings (length of comments, newline issues)

--- student_code.py
+++ student_code.py
@@ -1,6 +1,7 @@
 # DSCI321 Coding Assignment 1 - Charlie Zheng
+"""Robot Navigation Assignment - has a robot travel along given path in weighted graph"""
 
-# Defined below is the nested dictionary that contains all the nodes and their weights for each of the paths.
+# Defined below is the nested dictionary w/ nodes and weights.
 
 GRAPH = {
     "a": {"b":1, "d":5},
@@ -10,19 +11,24 @@ GRAPH = {
     "e": {"d":3, "i":2},
     "f": {"e":4, "g":3, "i":3},
     "g": {"h":2, "k":2},
-    "h": {"i":1, "j":2, "z": "4"},
+    "h": {"i":1, "j":2, "z": 4},
     "i": {"j":4, "k":2},
     "j": {"c":1, "k":3, "z":4},
-    "k": {"z":3}
+    "k": {"z":3},
+    "z": {}
 }
 
 def robot_navigation(nodes):
-    """This function takes a list of strings denoted as nodes and has a robot travel along it
-    The function weighs the paths that the robot takes and adds it up. It also checks for invalid node paths
+    """Function takes a list of strings as nodes and has a robot travel along it
+    Function weighs the paths that the robot takes ands sums. Also checks for invalid node paths
     """
     # Invalidity Check
     #  if nodes is a list
     if not isinstance(nodes, list):
+        return -1
+
+    # Check if nodes is not empty
+    if len(nodes) == 0:
         return -1
 
     # Check if nodes is all strings
@@ -43,23 +49,23 @@ def robot_navigation(nodes):
 
     for i in range(len(nodes) - 1):
         current = nodes[i]
-        next = nodes[i + 1]
+        next_node = nodes[i + 1]
 
 
         # Checks for cyclical nature, by checking current node against the set of visited nodes
-        # Also checks for cycles with the last node in the list being the one that causes it to be cyclical (given case) by checking against next.
-        if current in visited or next in visited:
+        # Also checks for cycles with the last node in the list causing it to be cyclical
+        if current in visited or next_node in visited:
             return -2
         visited.add(current)
 
 
-        # Checks for invalid paths (i.e node list {a, z})
-        if next not in GRAPH[current]:
+        # Checks for invalid paths (i.e. node list {a, z})
+        if next_node not in GRAPH[current]:
             return -1
 
-        total_weight += GRAPH[current][next]
+        total_weight += GRAPH[current][next_node]
 
 
     if nodes[-1] == "z":
-        return (total_weight, "z")
-    return 0
+        return total_weight, "z"
+    return 0, nodes[-1]
