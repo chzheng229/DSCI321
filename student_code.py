ad163fa501cedd7d97fc7ef1f41379cb155cf799
chzheng229 <chz229@lehigh.edu>
2026-09-05 13:52:44 -0400

    autograder fixes
    fixed some stuff for the autograder- should be letters not indices

--- student_code.py
+++ student_code.py
@@ -8,12 +8,13 @@ def part_1_graph():
     nodes_dict = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4}
     graph = [set() for _ in range (len(nodes_dict))]
 
-    graph[nodes_dict['a']].add(nodes_dict['b'])
-    graph[nodes_dict['a']].add(nodes_dict['e'])
-    graph[nodes_dict['b']].add(nodes_dict['c'])
-    graph[nodes_dict['c']].add(nodes_dict['d'])
-    graph[nodes_dict['c']].add(nodes_dict['e'])
-    graph[nodes_dict['d']].add(nodes_dict['b'])
+    # adding all the paths to sets
+    graph[nodes_dict['a']].add('b')
+    graph[nodes_dict['a']].add('e')
+    graph[nodes_dict['b']].add('c')
+    graph[nodes_dict['c']].add('d')
+    graph[nodes_dict['c']].add('e')
+    graph[nodes_dict['d']].add('b')
 
     return graph
 
@@ -24,14 +25,14 @@ def part_2_graph():
     graph = [[] for _ in range (len(nodes_dict))]
 
     # Adding all the node paths into lists using .append (different methods for lists vs sets.)
-    graph[nodes_dict['a']].append(nodes_dict['a'])
-    graph[nodes_dict['a']].append(nodes_dict['b'])
-    graph[nodes_dict['a']].append(nodes_dict['e'])
-    graph[nodes_dict['b']].append(nodes_dict['c'])
-    graph[nodes_dict['c']].append(nodes_dict['a'])
-    graph[nodes_dict['c']].append(nodes_dict['d'])
-    graph[nodes_dict['c']].append(nodes_dict['e'])
-    graph[nodes_dict['e']].append(nodes_dict['d'])
+    graph[nodes_dict['a']].append('a')
+    graph[nodes_dict['a']].append('b')
+    graph[nodes_dict['a']].append('e')
+    graph[nodes_dict['b']].append('c')
+    graph[nodes_dict['c']].append('a')
+    graph[nodes_dict['c']].append('d')
+    graph[nodes_dict['c']].append('e')
+    graph[nodes_dict['e']].append('d')
 
     return graph
 
@@ -42,12 +43,12 @@ def part_3_graph():
     graph = [{} for _ in range (len(nodes_dict))]
 
     # adding all the paths to the dictionaries - also assigning the weights for the paths
-    graph[nodes_dict['a']][nodes_dict['a']] = 8
-    graph[nodes_dict['a']][nodes_dict['b']] = 1
-    graph[nodes_dict['a']][nodes_dict['e']] = 4
-    graph[nodes_dict['b']][nodes_dict['c']] = 3
-    graph[nodes_dict['c']][nodes_dict['a']] = 2
-    graph[nodes_dict['c']][nodes_dict['e']] = 4
+    graph[nodes_dict['a']]['a'] = 8
+    graph[nodes_dict['a']]['b'] = 1
+    graph[nodes_dict['a']]['e'] = 4
+    graph[nodes_dict['b']]['c'] = 3
+    graph[nodes_dict['c']]['a'] = 2
+    graph[nodes_dict['c']]['e'] = 4
 
     return graph
 
@@ -88,4 +89,3 @@ def part_5_graph():
     graph['e']['a'] = 6
 
     return graph
-
